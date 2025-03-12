11c1d7d7e194914a1cfba92654a66f5b2a5de57a
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-03-12 02:52:43 -0500

    Add files via upload

--- QUANTUM NANO/QN_SOURCE.py
+++ QUANTUM NANO/QN_SOURCE.py
@@ -13,43 +13,80 @@ class QN(ShowBase):
   props=WindowProperties()
   props.setCursorHidden(True)
   props.setSize(1600,900)
-  props.setTitle("QUANTUM NANO")
-  s.win.requestProperties(props)
   s.keymap={}
   for k in "wsadeq":
    s.accept(k,s.set_key,[k,1])
    s.accept(f"{k}-up",s.set_key,[k,0])
    s.accept("escape",sys.exit)
-   s.spd=5
-   s.cam.setPos(32,32,66)
-   s.win.movePointer(0,s.win.getXSize()//2,s.win.getYSize()//2)
-   s.accept("mouse1",s.center_mouse)
-   c=s.loader.loadModel("models/box")
-   s.cursor = s.aspect2d.attachNewNode(TextNode("cursor"))
-   s.cursor.node().setText("Q")
-   s.cursor.node().setTextColor(1,1,1,1)
-   s.cursor.setScale(0.15)
-   s.taskMgr.add(s.update_cursor,"update_cursor")
-  if c:
-   T=[[[[a%256,b%256,a^b%256,255]for b in range(256)]for a in range(256)],[[[a%256,b%256,(a+b)%256,255]for b in range(256)]for a in range(256)],[[[a%256,b%256,a*b%256,255]for b in range(256)]for a in range(256)],[[[255,0,0,255]for b in range(256)]for a in range(256)],[[[0,255,0,255]for b in range(256)]for a in range(256)],[[[0,0,255,255]for b in range(256)]for a in range(256)],[[[255,255,255,255]for b in range(256)]for a in range(256)]]
-   nT=[]
-   for i in range(7):
-    nT.append(bytes([c for a in T[i] for b in a for c in b]))
-   for x in range(64):
-    for y in range(64):
-     for z in range(64):
-      if random.randint(1,16)==1:
-       N=NodePath("N")
-       c.copyTo(N)
-       N.reparentTo(s.render)
-       N.setPos(x,y,z)
-       t=Texture()
-       t.setup2dTexture(256,256,Texture.TUnsignedByte,Texture.FRgba8)
-       t.setRamImage(random.choice(nT))
-       ts=TextureStage("ts")
-       N.setTexture(ts,t)
+  s.spd=5
+  s.cam.setPos(8,8,18)
+  s.win.movePointer(0,s.win.getXSize()//2,s.win.getYSize()//2)
+  s.accept("mouse1",s.center_mouse)
+  s.cursor = s.aspect2d.attachNewNode(TextNode("cursor"))
+  s.cursor.node().setText("Q")
+  s.cursor.node().setTextColor(1,1,1,1)
+  s.cursor.setScale(0.15)
+  s.taskMgr.add(s.update_cursor,"update_cursor")
   s.taskMgr.add(s.update,"update")
   s.taskMgr.add(s.mouse_control,"mouse_control")
+  Cube=s.loader.loadModel("models/box")
+  if Cube:
+   diamond=bytes([c for a in [[[255,255,0,255]for b in range(1)]for a in range(1)] for b in a for c in b])
+   gold=bytes([c for a in [[[0,255,255,255]for b in range(1)]for a in range(1)] for b in a for c in b])
+   grass=bytes([c for a in [[[0,255,0,255]for b in range(1)]for a in range(1)] for b in a for c in b])
+   lapis=bytes([c for a in [[[255,0,0,255]for b in range(1)]for a in range(1)] for b in a for c in b])
+   redstone=bytes([c for a in [[[0,0,255,255]for b in range(1)]for a in range(1)] for b in a for c in b])
+   diamond_c=0
+   gold_c=0
+   grass_c=0
+   lapis_c=0
+   redstone_c=0
+   for x in range(16):
+    for y in range(16):
+     for z in range(16):
+      if z>=14:
+       nT=grass
+       grass_c+=1
+      elif z >= 12 and z < 14:
+       rand=random.randint(1,3)
+       if rand==1:
+        nT=grass
+        grass_c+=1
+       elif rand==2 or rand==3:
+        nT=lapis
+        lapis_c+=1
+      elif z>4 and z<12:
+       rand=random.randint(1,4)
+       if rand==1 or rand==2:
+        nT=grass
+        grass_c+=1
+       elif rand==3:
+        nT=gold
+        gold_c+=1
+       elif rand==4:
+        nT=redstone
+        redstone_c+=1
+      elif z>=0 and z<=4:
+       rand=random.randint(1,24)
+       if rand>=1 and rand<=23:
+        nT=grass
+        grass_c+=1
+       else:
+        nT=diamond
+        diamond_c+=1
+      N=render.attachNewNode("instance")
+      N.setPos(x,y,z)
+      t=Texture()
+      t.setup2dTexture(1,1,Texture.TUnsignedByte,Texture.FRgba8)
+      t.setRamImage(nT)
+      ts=TextureStage("ts")
+      N.setTexture(ts,t)
+      Cube.instanceTo(N)
+   render.flattenStrong()
+   props.setTitle(f"QUANTUM NANO | DIAMOND: {diamond_c}, GOLD: {gold_c}, GRASS: {grass_c}, LAPIS:{lapis_c}, REDSTONE: {redstone_c}")
+   s.win.requestProperties(props)
+  else:
+      sys.exit()
  def set_key(s,key,value):
   s.keymap[key]=value
  def update(s,task):
